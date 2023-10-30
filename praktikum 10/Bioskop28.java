19b852c0882e51b7c4895ff629d7932f3ccd0322
merasifa <merasifa@gmail.com>
2023-10-30 13:16:21 +0700

    for loop

--- praktikum 10/Bioskop28.java
+++ praktikum 10/Bioskop28.java
@@ -3,14 +3,14 @@ public class Bioskop28 {
         
         String [][] penonton = new String[4][2];
 
-        // penonton [0][0] = "Amin";
-        // penonton [0][1] = "Bena";
-        // penonton [1][0] = "Candra";
-        // penonton [1][1] = "Dela";
-        // penonton [2][0] = "Eka";
-        // penonton [2][1] = "Farhan";
-        // penonton [3][0] = "Gisel";
-        // penonton [3][1] = "Hana";
+        penonton [0][0] = "Amin";
+        penonton [0][1] = "Bena";
+        penonton [1][0] = "Candra";
+        penonton [1][1] = "Dela";
+        penonton [2][0] = "Eka";
+        penonton [2][1] = "Farhan";
+        penonton [3][0] = "Gisel";
+        penonton [3][1] = "Hana";
         System.out.println(penonton.length);
         for (int i = 0; i < penonton.length; i++){ // menamapilkan panjang setiap baris pd array menggunkan for loop
             System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
@@ -27,10 +27,17 @@ public class Bioskop28 {
             System.out.println("Panjang baris: " + barisPenonton.length);
         }
 
+        
+
         System.out.println(penonton.length);
         System.out.println(penonton[0].length);
         System.out.println(penonton[1].length);
         System.out.println(penonton[2].length);
         System.out.println(penonton[3].length);
+
+        System.out.println("Penonton pada baris ke-3: ");
+         for (int i = 0; i < penonton[2].length; i++){
+            System.out.println(penonton[2][i]);
+         }
     }
 }
