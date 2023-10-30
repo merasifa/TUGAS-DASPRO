42dbcab7a95898b0e88ef595118e9b841aed2bc2
merasifa <merasifa@gmail.com>
2023-10-30 12:54:22 +0700

    forreach loop

--- praktikum 10/Bioskop28.java
+++ praktikum 10/Bioskop28.java
@@ -12,15 +12,20 @@ public class Bioskop28 {
         // penonton [3][0] = "Gisel";
         // penonton [3][1] = "Hana";
         System.out.println(penonton.length);
-        for (int i = 0; i < penonton.length; i++){
+        for (int i = 0; i < penonton.length; i++){ // menamapilkan panjang setiap baris pd array menggunkan for loop
             System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
         }
 
 
-        System.out.printf("%s \t %s\n", penonton[0][0], penonton [0][1]);
-        System.out.printf("%s \t %s\n", penonton[1][0], penonton [1][1]);
-        System.out.printf("%s \t %s\n", penonton[2][0], penonton [2][1]);
-        System.out.printf("%s \t %s\n", penonton[3][0], penonton [3][1]);
+        // System.out.printf("%s \t %s\n", penonton[0][0], penonton [0][1]);
+        // System.out.printf("%s \t %s\n", penonton[1][0], penonton [1][1]);
+        // System.out.printf("%s \t %s\n", penonton[2][0], penonton [2][1]);
+        // System.out.printf("%s \t %s\n", penonton[3][0], penonton [3][1]);
+
+        System.out.println(penonton.length);
+        for(String[] barisPenonton : penonton){
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
 
         System.out.println(penonton.length);
         System.out.println(penonton[0].length);
