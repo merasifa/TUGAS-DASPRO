315333be9a05549c117f1759d101e9e3499f28fc
merasifa <merasifa@gmail.com>
2023-11-12 23:33:26 +0700

    penambahan detail input

--- praktikum 11/Quis28.java
+++ praktikum 11/Quis28.java
@@ -10,12 +10,22 @@ public class Quis28 {
         do{
             int number = random.nextInt(10) + 1;
             boolean success = false;
-        do {
-            System.out.print("Tebak angka (1-10): ");
-            int answer = input.nextInt();
-            input.nextLine();
-        success = (answer == number);
+            do {
+                System.out.print("Tebak angka (1-10): ");
+                int answer = input.nextInt();
+                input.nextLine();
+
+                if (answer == number) {
+                    System.out.println("Jawaban Anda Benar");
+                    success = true;
+                } else if (answer < number) {
+                System.out.println("Jawaban terlalu kecil. Coba Lagi"); 
+                } else {
+                    System.out.println("Jawaban terlalu besar. Coba Lag");
+                }
+
         } while(!success);
+
             System.out.print("Apakah Anda ingin mengulang permainan (Y/y)?");
             menu = input.nextLine().charAt(0);
         } while(menu=='y' || menu=='Y'); //program akan terus berjalan dalam loop selama pengguna ingin bermain lagi 
