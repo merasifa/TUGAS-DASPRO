e35a6be24ab6004cd8b98d7b3c0c84564b9a2f6a
merasifa <merasifa@gmail.com>
2023-11-13 14:03:43 +0700

    BUBBLE SHORT

--- praktikum 11/BubbleShort28.java
+++ praktikum 11/BubbleShort28.java
@@ -1,7 +1,7 @@
 public class BubbleShort28 {
     public static void main(String[] args) {
         
-        String [][] atlet = {
+        String [][] atlet = { // inisialisasi array
             {"Ibnu", "Marco", "Faiz", "Agung", "Vira"}, // Badminton
             {"Mera", "Hana", "Rea", "Reza", "Isna"}, // Tenis Meja
             {"Dimas", "Farhan", "Vincent", "Gaby", "Batavia"}, // Basket
@@ -10,13 +10,25 @@ public class BubbleShort28 {
 
         String [] cabor = {"Badminton", "Tenis Meja", "Basket", "Bola Voli"};
 
-        for (int i = 0; i < atlet.length; i++) { // perulangan menampilkan cabor
-            System.out.println(cabor[i] + ":");
+        for (int i = 0; i < atlet.length; i++) {
+            for (int j = 0; j < atlet[i].length - 1; j++) {
+                for (int k = 0; k < atlet[i].length - j - 1; k++) {
+                    if (atlet[i][k].compareToIgnoreCase(atlet[i][k + 1]) > 0) {
+                        String temp = atlet[i][k];
+                        atlet[i][k] = atlet[i][k + 1];
+                        atlet[i][k + 1] = temp;
+                    }
+                }
+            }
+        }
 
-            for ( int j = 0; j < atlet[i].length; j++) { // perulangan menampilkan atlet
-                System.out.println("Atlet ke -" + (j+1) + " adalah " + atlet[i][j]);
+        for (int i = 0; i < atlet.length; i++) {
+            System.out.println( cabor[i] );
+            for (int j = 0; j < atlet[i].length; j++) {
+                System.out.println("Atlet ke -" + (j + 1) + " adalah " + atlet[i][j]);
             }
             System.out.println();
         }
     }
 }
+
